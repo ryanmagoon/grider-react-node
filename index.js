7642e8096bed3613209e1863b7e45e61f3c7edb7
Ryan Magoon <ryan.magoon@advance-auto.com>
2022-02-01 22:54:36 -0500

    wire up auth

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session')
 const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys.js')
+require('./models/User')
+require('./services/passport')
+
+mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-passport.use(new GoogleStrategy())
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+require('./routes/authRoutes')(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
