d5fea860b21579ba7f3f3f6d9e6626b1c53f2d0f
Ryan Magoon <mr.magoon5@gmail.com>
2022-02-05 12:07:18 -0500

    add env to gitignore

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
     res.send(req.user)
   })
 
-  app.get('/api/current_user', (req, res) => {
-    res.send(req.user)
+  app.get('/api/current_user', ({ user }, res) => {
+    res.send(user)
   })
 }
